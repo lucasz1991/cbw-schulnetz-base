a943c3dec3aebe7bd94790019942ae41b6ba32e3
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-28 06:26:54 +0200

    fix: update navigation link for absences to point to absences-create

--- resources/views/livewire/user-navigation-menu.blade.php
+++ resources/views/livewire/user-navigation-menu.blade.php
@@ -294,7 +294,7 @@
              
                                              Qualiprogramm
                                          </x-nav-link>
-                                         <x-nav-link href="/absences" wire:navigate  :active="request()->is('/absences')">
+                                         <x-nav-link href="/absences-create" wire:navigate  :active="request()->is('/absences-create')">
                                                 <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 8v4l2.5 2.5M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18Z"/>
                                                 </svg>
