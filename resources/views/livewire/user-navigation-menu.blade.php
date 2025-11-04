b6e464fdb7b088a4878eedaedb519e2943e4e2ca
Lucas Zacharias <lucas@zacharias-net.de>
2025-11-04 13:00:13 +0100

    dfdf

--- resources/views/livewire/user-navigation-menu.blade.php
+++ resources/views/livewire/user-navigation-menu.blade.php
@@ -191,9 +191,9 @@
                                                         </svg>
                                                         {{ __('Profil') }}
                                                     </x-nav-link>
-                                                    <form method="POST" action="{{ route('logout') }}" x-data>
-                                                        @csrf
-                                                        <x-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                                    <form method="POST" action="{{ route('logout') }}">
+    @csrf
+    <x-nav-link as="button" type="submit">
                                                             <svg class="w-5 h-5  mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                                                             </svg>
