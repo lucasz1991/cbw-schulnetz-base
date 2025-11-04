93657f04005819a3bb5d3e1b101012ead212c2a1
Lucas Zacharias <lucas@zacharias-net.de>
2025-11-04 13:22:14 +0100

    dfdf

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -59,15 +59,14 @@
                                     </x-dropdown-link>
                                     
                                     <div class="border-t border-gray-200"></div>
-                                                                                        <form method="POST" action="{{ route('logout') }}" wire:ignore>
-                                                            @csrf
-                                                            <x-nav-link as="button" type="submit">
-                                                            <svg class="w-5 h-5  mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                                                            </svg>
-                                                            {{ __('Abmelden') }}
-                                                        </x-nav-link>
-                                                    </form>
+                                    <form method="POST" action="{{ route('logout') }}" x-data>
+                                        @csrf
+                                        <x-dropdown-link  as="button" type="submit">
+                                            <svg class="w-5 h-5  mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                            </svg>
+
+                                            {{ __('Abmelden') }}
                                         </x-dropdown-link>
                                     </form>
                                 </x-slot>
