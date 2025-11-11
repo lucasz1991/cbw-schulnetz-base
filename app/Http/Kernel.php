552c15264ceb3e3d3c0ef35f616bd895b350dada
Lucas Zacharias <lucas@zacharias-net.de>
2025-11-11 02:26:08 +0100

    USer status Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\LogActivity::class,
             \App\Http\Middleware\MaintenanceMode::class,
+            \App\Http\Middleware\CheckUserStatus::class,
         ],
 
         'api' => [
