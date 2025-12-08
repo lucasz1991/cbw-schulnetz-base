624a126b99f90bf1d09812dedd2b2e15f5a4c922
Lucas Zacharias <lucas@zacharias-net.de>
2025-12-08 11:27:43 +0100

    dfdf

--- app/Jobs/ApiUpdates/CheckPersonsCourses.php
+++ app/Jobs/ApiUpdates/CheckPersonsCourses.php
@@ -21,8 +21,8 @@ class CheckPersonsCourses implements ShouldQueue, ShouldBeUniqueUntilProcessing
     public $backoff = [10, 60, 180];
 
     // Fenster konfigurieren: Vergangenheit/Zukunft
-    private const PAST_YEARS   = 1; // ab jetzt -1 Jahr
-    private const FUTURE_YEARS = 1; // bis jetzt +1 Jahr
+    private const PAST_YEARS   = 2; // ab jetzt -1 Jahr
+    private const FUTURE_YEARS = 2; // bis jetzt +1 Jahr
 
     public function __construct(public int $personPk) {}
 
