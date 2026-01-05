a3d8f4ded149c455cf318887a9f2e764d63d9e15
Lucas Zacharias <lucas@zacharias-net.de>
2026-01-05 05:40:28 +0100

    sdsd

--- app/Livewire/User/ReportBook.php
+++ app/Livewire/User/ReportBook.php
@@ -60,7 +60,7 @@ public ?int $selectedCourseDayId = null; // aktueller Kurs-Tag
 
     public int $editorVersion = 0;
 
-    protected $listeners = [ 'reportbook.entry.updated' => 'loadCurrentEntry' ];
+    protected $listeners = [ 'reportbook.entry.updated' => 'reloadCurrentEntry' ];
 
 public function mount(): void
 {
