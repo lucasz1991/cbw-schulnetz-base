13b6908e2613b76989134d805abd611fbcc7d078
Lucas Zacharias <lucas@zacharias-net.de>
2026-01-07 06:07:27 +0100

    dfdfdf

--- app/Livewire/Tools/Ai/ReportBookAiAssistant.php
+++ app/Livewire/Tools/Ai/ReportBookAiAssistant.php
@@ -201,7 +201,7 @@ class ReportBookAiAssistant extends Component
         } 
 
         $this->entry->text = $textToSave;
-        $this->entry->status = 1;
+        $this->entry->status = 0; // entwurf
         $this->entry->save();
         $this->dispatch('updated');
         $this->showModal = false;
