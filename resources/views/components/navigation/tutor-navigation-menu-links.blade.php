775072f97c6a9fadf19b87a05234972db76201df
Lucas Zacharias <lucas@zacharias-net.de>
2026-01-19 22:34:17 +0100

    Update navigation links for consistency and remove unused links

--- resources/views/components/navigation/tutor-navigation-menu-links.blade.php
+++ resources/views/components/navigation/tutor-navigation-menu-links.blade.php
@@ -1,32 +1,16 @@
 
-        <x-nav-link href="/tutor-dashboard" wire:navigate  :active="request()->is('/tutor-dashboard')">
+        <x-nav-link href="/tutor/dashboard" wire:navigate  :active="request()->is('/tutor/dashboard')">
                 <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                     <rect x="4" y="4" width="16" height="16" rx="2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M8 8h8M8 12h8M8 16h4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             Dashboard
         </x-nav-link>
-        <x-nav-link href="/tutor-courses" wire:navigate  :active="request()->is('/tutor-courses')">
+        <x-nav-link href="/tutor/tutor-courses" wire:navigate  :active="request()->is('/tutor/tutor-courses')">
                 <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 8v4l2.5 2.5M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18Z"/>
                 </svg>
             Meine Kurse 
-        </x-nav-link>
-        <x-nav-link href="/makeup-exam-create" wire:navigate  :active="request()->is('/makeup-exam-create')">
-                <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <rect x="4" y="3" width="16" height="18" rx="2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M8 7h8M8 11h8M8 15h4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M16 3v2M8 3v2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            Kalender
-        </x-nav-link>
-        <x-nav-link href="/makeup-exam-create" wire:navigate  :active="request()->is('/makeup-exam-create')">
-                <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <rect x="4" y="3" width="16" height="18" rx="2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M8 7h8M8 11h8M8 15h4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M16 3v2M8 3v2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            News
         </x-nav-link>
         
         @php
