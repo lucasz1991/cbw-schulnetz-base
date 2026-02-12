a040584fcbc50c1c1e80122cf8245973b5afc60e
Lucas Zacharias <lucas@zacharias-net.de>
2026-02-12 13:16:15 +0100

    ghghg

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,18 +15,6 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('activity:clean-old')
             ->dailyAt('02:00');
-
-        // Schedule the shelf rentals check command to run at 9 AM, 4 PM, and 7 PM daily
-        // $schedule->command('shelf:check-rentals')->cron('0 9,16,19 * * *');
-
-        // Schedule the rental reminders command to run daily at 12:00 PM
-        // $schedule->command('rental:send-reminders')->dailyAt('12:00');
-
-        // Schedule the exports command to run monthly on the 1st at 2:00 AM
-        // $schedule->command('exports:run')->monthlyOn(1, '02:00');
-        // $schedule->command('uvs:update-persons')->dailyAt('01:46');
-
-
     }
 
     /**
