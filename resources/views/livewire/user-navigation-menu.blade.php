bfc17c849a055c7d654771b147efbbc05275ecc7
Lucas Zacharias <lucas@zacharias-net.de>
2026-05-07 14:17:10 +0200

    Remove unnecessary line break in user navigation menu for improved code cleanliness

--- resources/views/livewire/user-navigation-menu.blade.php
+++ resources/views/livewire/user-navigation-menu.blade.php
@@ -58,7 +58,6 @@
                          <div class="flex-none flex items-center h-full py-2 max-md:order-1" @click="$dispatch('navhide')">
                              <a href="{{ \App\Providers\RouteServiceProvider::home() }}" wire:navigate   class="h-full flex items-center max-sm:max-w-[160px]">
                                  <x-logo.site-logo-horizontal  />
-
                              </a>
                          </div>
                          <div class="flex items-center space-x-4 max-md:order-3 md:order-2  flex-none" @click="$dispatch('navhide')">
